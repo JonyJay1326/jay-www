242d9435dbd5c239c6c8c75f193e7d9969ee27b1
FlyingAir <827199132@qq.com>
2018-03-13 15:51:59 +0800

    :rocket: 修改主页loading样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,49 @@
 <template>
   <div class="main-container">
-  <banner></banner>
-  <div>{{msg}}</div>
+    <div class="nav-container">
+      <div v-for="navItem in navList" class="nav-item">
+        <a :href="navItem.url">{{navItem.name}}</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Banner from '../components/Banner'
 export default {
   name: "nav",
   data() {
     return {
-      msg: "hello"
+      navList: [
+        {
+          name: "Home",
+          url: "http://www.jonyjay.com"
+        },
+        {
+          name: "Blog",
+          url: "http://blog.jonyjay.com"
+        },
+        {
+          name: "MusicDemo",
+          url: "http://music.jonyjay.com"
+        },
+        {
+          name: "GitHub",
+          url: "https://github.com/JonyJay1326"
+        }
+      ]
     };
-  },
-
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="stylus">
+.nav-container {
+  display: flex;
 
+  .nav-item {
+    float: 1px;
+    padding: 20px;
+  }
+}
 </style>
